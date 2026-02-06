3481b25e3b901c3d6408d2157df751f868bad98a
Raghav <raghav.tandon13@gmail.com>
2026-02-06 12:25:05 +0530

    8

---
+++ 27.remove-element.cpp
@@ -0,0 +1,17 @@
+// @leet start
+class Solution {
+  public:
+    int removeElement(vector<int> &nums, int val) {
+        if (nums.empty())
+            return 0;
+        int c = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[c] = nums[i];
+                c++;
+            }
+        }
+        return c;
+    }
+};
+// @leet end

2158cf36f1bb78d306813fd5f2ca42895abb313b
Raghav <raghav.tandon13@gmail.com>
2026-02-07 00:10:54 +0530

    clean

--- 122.best-time-to-buy-and-sell-stock-ii.cpp
+++ 122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,8 +1,28 @@
+#include <vector>
+using namespace std;
+
 // @leet start
 class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        
+  public:
+    int maxProfit(vector<int> &prices) {
+
+        int maxProfit = 0;
+	vector<int> changes(prices.size());
+
+        for (int i = 1; i < prices.size(); i++) {
+            changes[i] = prices[i] - prices[i - 1];
+        }
+
+        for (int i = 0; i < changes.size(); i++) {
+            maxProfit += max(0, changes[i]);
+        }
+
+        // compact version
+        // for (int i = 1; i < prices.size(); i++) {
+        //     maxProfit += max(0, prices[i] - prices[i - 1]);
+        // }
+
+        return maxProfit;
     }
 };
 // @leet end

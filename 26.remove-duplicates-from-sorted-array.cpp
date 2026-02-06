aaedbadb6586b0516bca266b24f87540fb425067
Raghav <raghav.tandon13@gmail.com>
2026-02-06 12:25:00 +0530

    7

---
+++ 26.remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,19 @@
+// @leet start
+class Solution {
+  public:
+    int removeDuplicates(vector<int> &nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+
+        int c = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[c]) {
+                nums[c + 1] = nums[i];
+                c++;
+            }
+        }
+        return c + 1;
+    }
+};
+// @leet end

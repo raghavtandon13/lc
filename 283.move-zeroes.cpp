413ecd7a3ca56295ef5bf8283ad741580109845e
Raghav <raghav.tandon13@gmail.com>
2026-02-06 12:25:08 +0530

    9

---
+++ 283.move-zeroes.cpp
@@ -0,0 +1,17 @@
+// @leet start
+class Solution {
+  public:
+    void moveZeroes(vector<int> &nums) {
+        int c = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[c] = nums[i];
+                c++;
+            };
+        }
+        for (int i = c; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
+// @leet end

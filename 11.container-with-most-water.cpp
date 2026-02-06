8c872a1efedefb8beffb3a04a6c162ce662687bf
Raghav <raghav.tandon13@gmail.com>
2026-02-06 12:24:35 +0530

    2.

---
+++ 11.container-with-most-water.cpp
@@ -0,0 +1,40 @@
+// @leet start
+class Solution {
+  public:
+    // BRUTE FORCE
+    int maxArea_bf(vector<int> &height) {
+        int biggest = 0;
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i + 1; j < height.size(); j++) {
+
+                int w = j - i;
+                int h = height[i] > height[j] ? height[j] : height[i];
+                int area = h * w;
+
+                if (area > biggest) {
+                    biggest = area;
+                }
+            }
+        }
+
+        return biggest;
+    }
+
+    // 2 POINTER
+    int maxArea(vector<int> &height) {
+        int l = 0, r = height.size() - 1;
+        int best = 0;
+
+        while (l < r) {
+            int area = min(height[l], height[r]) * (r - l);
+            best = max(best, area);
+
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return best;
+    }
+};
+// @leet end

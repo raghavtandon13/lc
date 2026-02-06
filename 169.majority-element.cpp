aeaced00f9bd000f85760476fce9f4fb21040c9b
Raghav <raghav.tandon13@gmail.com>
2026-02-07 00:11:04 +0530

    13

---
+++ 169.majority-element.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// @leet start
+class Solution {
+  public:
+    int majorityElement(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+
+        int e = nums[0];
+        int c = 1;
+
+        int max_e = nums[0];
+        int max_c = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == e) {
+                c++;
+            } else {
+                if (c > max_c) {
+                    max_e = e;
+                    max_c = c;
+                }
+                e = nums[i];
+                c = 1;
+            }
+        }
+
+        if (c > max_c) {
+            max_c = c;
+            max_e = e;
+        }
+
+        return max_e;
+    }
+};
+// @leet end

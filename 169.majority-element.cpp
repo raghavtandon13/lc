085c0300440c5cfd89a7708044bacfbb89a07077
Raghav <raghav.tandon13@gmail.com>
2026-02-15 21:48:56 +0530

    14

--- 169.majority-element.cpp
+++ 169.majority-element.cpp
@@ -1,38 +1,54 @@
-#include <algorithm>
+// #include <algorithm>
 #include <vector>
 using namespace std;
 
 // @leet start
 class Solution {
+    // [6,5,5]
+    // [2,2,1,1,1,2,2]
   public:
     int majorityElement(vector<int> &nums) {
-        sort(nums.begin(), nums.end());
-
-        int e = nums[0];
-        int c = 1;
-
-        int max_e = nums[0];
-        int max_c = 1;
-
+        int e = nums[0]; // 2
+        int c = 1;       // -1
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i] == e) {
                 c++;
             } else {
-                if (c > max_c) {
-                    max_e = e;
-                    max_c = c;
+                c--;
+                if (c == -1) {
+                    e = nums[i];
+                    c = 1;
                 }
-                e = nums[i];
-                c = 1;
             }
         }
+        return e;
 
-        if (c > max_c) {
-            max_c = c;
-            max_e = e;
-        }
-
-        return max_e;
+        // sort(nums.begin(), nums.end());
+        //
+        // int e = nums[0];
+        // int c = 1;
+        // int max_e = nums[0];
+        // int max_c = 1;
+        //
+        // for (int i = 1; i < nums.size(); i++) {
+        //     if (nums[i] == e) {
+        //         c++;
+        //     } else {
+        //         if (c > max_c) {
+        //             max_e = e;
+        //             max_c = c;
+        //         }
+        //         e = nums[i];
+        //         c = 1;
+        //     }
+        // }
+        //
+        // if (c > max_c) {
+        //     max_c = c;
+        //     max_e = e;
+        // }
+        //
+        // return max_e;
     }
 };
 // @leet end
--- 283.move-zeroes.cpp
+++ 283.move-zeroes.cpp
@@ -1,19 +1,23 @@
+#include <iostream>
 #include <vector>
 using namespace std;
+
+// intuition: first zeros avalable for non zero values to be moved to
 
 // @leet start
 class Solution {
   public:
     void moveZeroes(vector<int> &nums) {
-        int c = 0;
+        int write = 0;
+
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
-                nums[c] = nums[i];
-                c++;
-            };
+                nums[write++] = nums[i];
+            }
         }
-        for (int i = c; i < nums.size(); i++) {
-            nums[i] = 0;
+
+        while (write < nums.size()) {
+            nums[write++] = 0;
         }
     }
 };
